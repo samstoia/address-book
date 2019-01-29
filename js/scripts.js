4e90a1b037ffcb6876dc63ea093d545fc89237fe
Guest User <Guest@epicodus-41.local>
2019-01-29 11:58:23 -0800

    Do all the things

--- js/scripts.js
+++ js/scripts.js
@@ -38,11 +38,29 @@ AddressBook.prototype.deleteContact = function(id) {
 }
 
 // Business Logic for Contacts ---------
-function Contact(firstName, lastName, phoneNumber) {
+function Contact(firstName, lastName, phoneNumber, email, homeAddress, workAddress) {
   this.firstName = firstName,
   this.lastName = lastName,
-  this.phoneNumber = phoneNumber
-}
+  this.phoneNumber = phoneNumber,
+  this.email = email,
+  this.homeAddress = homeAddress;
+  this.workAddress = workAddress;
+  };
+
+  function WorkAddress(street, city, state, zip) {
+    this.street = street,
+    this.city = city,
+    this.state = state,
+    this.zip = zip
+
+  }
+
+  function HomeAddress(street, city, state, zip) {
+    this.street = street,
+    this.city = city,
+    this.state = state,
+    this.zip = zip
+  }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
@@ -66,6 +84,16 @@ function showContact(contactId) {
   $(".first-name").html(contact.firstName);
   $(".last-name").html(contact.lastName);
   $(".phone-number").html(contact.phoneNumber);
+  $(".email").html(contact.email);
+  $(".home-street").html(contact.homeAddress.street);
+  $(".home-city").html(contact.homeAddress.city);
+  $(".home-state").html(contact.homeAddress.state);
+  $(".home-zip").html(contact.homeAddress.zip);
+  $(".work-street").html(contact.workAddress.street);
+  $(".work-city").html(contact.workAddress.city);
+  $(".work-state").html(contact.workAddress.state);
+  $(".work-zip").html(contact.workAddress.zip);
+  // $(".work-address").html(contact.address.workAddress);
   var buttons = $("#buttons");
   buttons.empty();
   buttons.append("<button class='deleteButton' id=" +  + contact.id + ">Delete</button>");
@@ -92,8 +120,20 @@ $(document).ready(function() {
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var inputtedPhoneNumber = $("input#new-phone-number").val();
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    var inputtedEmail = $("input#new-email").val();
+    var inputtedHomeStreet = $("input#new-home-street").val();
+    var inputtedHomeCity = $("input#new-home-city").val();
+    var inputtedHomeState = $("input#new-home-state").val();
+    var inputtedHomeZip = $("input#new-home-zip").val();
+    var inputtedWorkStreet = $("input#new-work-street").val();
+    var inputtedWorkCity = $("input#new-work-city").val();
+    var inputtedWorkState = $("input#new-work-state").val();
+    var inputtedWorkZip = $("input#new-work-zip").val();
+    var newHomeAddress = new HomeAddress(inputtedHomeStreet, inputtedHomeCity, inputtedHomeState, inputtedHomeZip);
+    var newWorkAddress = new WorkAddress(inputtedWorkStreet, inputtedWorkCity, inputtedWorkState, inputtedWorkZip)
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmail, newHomeAddress, newWorkAddress);
     addressBook.addContact(newContact);
     displayContactDetails(addressBook);
+    console.log(newContact);
   });
 });
